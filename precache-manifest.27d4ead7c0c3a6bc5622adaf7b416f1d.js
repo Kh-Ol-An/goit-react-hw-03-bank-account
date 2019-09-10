527978da5e75ac0cf9ec07dcda0d64d348ed833b
Kh-Ol-An <olegkhristenko@gmail.com>
2019-09-10 12:53:46 +0300

    Updates

---
+++ precache-manifest.27d4ead7c0c3a6bc5622adaf7b416f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ae7361af4baa99075600abf1d376cf",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "1247a013acac71b9223d",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "418547bf31c3456015fb",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.022e6c2f.chunk.css"
+  },
+  {
+    "revision": "1247a013acac71b9223d",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.dd213bad.chunk.js"
+  },
+  {
+    "revision": "418547bf31c3456015fb",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.923729ef.chunk.js"
+  },
+  {
+    "revision": "2ce760ce333fda087249",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.8add6ca6.js"
+  }
+]);
